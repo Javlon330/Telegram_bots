0c628230e39022d27df6a91b7a31da70288504c4
Javlon330 <92207619+Javlon330@users.noreply.github.com>
2021-10-21 15:24:22 +0500

    Update Telegram_bot.py

--- Telegram_bot.py
+++ Telegram_bot.py
@@ -2,16 +2,16 @@ from transliterate import to_cyrillic, to_latin
 Token = '2070214263:AAFsnDYqNDYay3lknPqC_L4Tb9Br2Z275sY'
 import telebot
 bot = telebot.TeleBot(Token, parse_mode=None)
-sozlar = ['salom','hayr']
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Assalom aleykum.Botimizga Hush kelibsiz!!!")
+	bot.reply_to(message, "Salom.")
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
 	msg = message.text
-	if msg.isascii():
-		javob = to_cyrillic(msg)
-	else:
-		javob = to_latin(msg)
-	bot.reply_to(message, javob )
+	if msg == 'salom':
+		javob = 'hayr'
+		bot.reply_to(message, javob)
+	elif msg == 'hayr':
+		javob = "Ko'rishguncha"
+		bot.reply_to(message, javob)
 bot.polling()
